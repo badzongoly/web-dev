c3a5e4c8637c3ae3cc5fb73b89377cf731c85329
badzongoly <badzongoly@gmail.com>
2026-02-17 13:00:38 +0100

    week2 session1

--- js-basics/app.js
+++ js-basics/app.js
@@ -1,1 +1,23 @@
 console.log("Hello World!");
+const name = "John";
+console.log("My name is " + name);
+console.log(`My name is ${name}`);
+
+let x = -10;
+console.assert(x > 10, "x should be 10");
+const students = [  { name: 'Asha', country: 'Nepal', age: 20 },  
+{ name: 'Rafi', country: 'Bangladesh', age: 22 },  
+{ name: 'Eugene', country: 'Ghana', age: 30 }
+];
+console.table(students);
+
+
+console.time('loop');
+
+let total = 0;
+for (let i = 0; i < 1000000; i++) {
+  total += i;
+}
+
+console.timeEnd('loop');
+console.log('total =', total);
